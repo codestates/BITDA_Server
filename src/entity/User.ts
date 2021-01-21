618d5741e1c4bab1a805429ce70ce6b8c79bbb19
qkrwogns9703 <jaehunb350@gmail.com>
2021-01-21 19:39:56 +0900

    Add bookmark table

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -6,51 +6,51 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
   DeleteDateColumn,
-  ManyToMany,
-  JoinTable,
   OneToMany,
 } from 'typeorm';
-import Drink from './Drinks';
+
 import Review from './Review';
+import Bookmark from './Bookmark';
 @Entity()
 export default class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
+
   @Column()
   userName: string;
+
   @Column()
   email: string;
+
   @Column({ default: 'localUser' })
   socialId: string;
+
   @Column({ default: 'socialPassword' })
   password: string;
+
   @Column({ default: 'noPath' })
   userImage: string;
+
   @Column({ default: 'local' })
   provider: string;
+
   @Column({ default: 0 })
   admin: number;
+
   @CreateDateColumn()
   createdAt: Date;
+
   @UpdateDateColumn()
   updatedAt: Date;
+
   @DeleteDateColumn()
   deletedAt: Date;
-  @ManyToMany((type) => Drink)
-  @JoinTable({
-    name: 'drinks_bookmark',
-    joinColumn: {
-      name: 'userId',
-      referencedColumnName: 'id',
-    },
-    inverseJoinColumn: {
-      name: 'drinkId',
-      referencedColumnName: 'id',
-    },
-  })
-  drinks: Drink[];
+
   @OneToMany((type) => Review, (review) => review.user)
   review: Review[];
+
+  @OneToMany((type) => Bookmark, (bookmark) => bookmark.user)
+  bookmark: Bookmark[];
 
   static async localRegister(
     email: string,
@@ -130,7 +130,6 @@ export default class User extends BaseEntity {
       .leftJoin('user.drinks', 'drinks')
       .where('user.id = :id', { id })
       .getOne();
-
     return drinks;
   }
 }
