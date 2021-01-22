8be6be9f248c69a4ec612a12e222e2fe31d9c504
qkrwogns9703 <jaehunb350@gmail.com>
2021-01-22 11:35:41 +0900

    Add bookmark interface

--- src/controller/drinks/typeList.ts
+++ src/controller/drinks/typeList.ts
@@ -1,49 +1,19 @@
-import { Request, Response } from 'express';
-import { Connection, getConnection } from 'typeorm';
+import { NextFunction, Request, Response } from 'express';
+import { FindOperator } from 'typeorm';
 import Drink from '../../entity/Drinks';
-export default async (req: Request, res: Response): Promise<void> => {
-
+export default async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
   try {
-    const filteredWeaklist = await Drink.weakList(
-      req.body.alcohol,
-      req.body.type,
-      req.body.price,
-      req.body.taste,
-     
-     );
-    const filteredStronglist = await Drink.strongList(
-      req.body.alcohol,
-      req.body.type,
-      req.body.price,
-      req.body.taste,
-    
-    ); 
-	if (req.body.alcohol === '도수 있는 편') {
-		filteredStronglist.map((data) => {
-		  delete data.type;
-		  delete data.price;
-		  delete data.taste;
-		  delete data.ingredient;
-		  delete data.origin;
-		  delete data.url;
-		  delete data.desc;
-		});
-       res.send(filteredStronglist);
-	  } else if(req.body.alcohol === '도수 낮은 편') {
-	  filteredWeaklist.map((data) => {
-        delete data.type;
-        delete data.price;
-        delete data.taste;
-        delete data.ingredient;
-        delete data.origin;
-        delete data.url;
-        delete data.desc;
-      });
-     res.send(filteredWeaklist);
-    }
-   
-    
+    console.log(req.body);
+
+    const whereObj: object = {
+      type: '탁주/막걸리',
+      price: '1~2만원',
+    };
   } catch (err) {
-    res.send({ message: 'error message ' });
+    next(err);
   }
 };
--- src/controller/users/bookmark.ts
+++ src/controller/users/bookmark.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import Bookmark from '../../entity/Bookmark';
-import { UserData } from '../../definitions';
+import { BookmarkDrinks } from '../../definitions';
+
 export default async (
   req: Request,
   res: Response,
@@ -8,7 +9,9 @@ export default async (
 ): Promise<void | Response> => {
   try {
     const id: number = res.locals.decodedId;
-    const drinks: object[] = await Bookmark.bookMarkList(id);
+    const drinks: BookmarkDrinks[] = await Bookmark.bookMarkList(id);
+
+    console.log(drinks[0].drink.id);
     res.status(200).send({ drinks });
   } catch (err) {
     next(err);
