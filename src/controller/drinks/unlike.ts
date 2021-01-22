f88e255b60616b4fc0392dfbce209ba12cc10e8f
rameau17 <yoojin.rameau@gmail.com>
2021-01-22 15:06:26 +0900

    drink add immplement

--- src/controller/drinks/unlike.ts
+++ src/controller/drinks/unlike.ts
@@ -1,5 +1,11 @@
 import { Request, Response } from 'express';
-
+import Bookmark from '../../entity/Bookmark';
 export default async (req: Request, res: Response): Promise<void> => {
-	console.log('unlike');
+	try {
+		const id = res.locals.decodedId;
+        await Bookmark.removeLike(id, req.body.drinkId)
+		res.send({ bookMark: false })
+	  } catch (err) {
+		res.send({ message: 'error message' });
+	  }
 };
