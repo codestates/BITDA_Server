58d36fb1bdc2fda789438615c1b070594d157405
qkrwogns9703 <jaehunb350@gmail.com>
2021-01-25 13:30:22 +0900

    Modify config_file

--- ormconfig.ts
+++ ormconfig.ts
@@ -9,12 +9,12 @@ module.exports = {
   database: process.env.DATABASE_NAME,
   synchronize: true,
   logging: false,
-  entities: ['src/entity/**/*.ts'],
-  migrations: ['src/migration/**/*.ts'],
-  subscribers: ['src/subscriber/**/*.ts'],
+  entities: ['dist/entity/**/*.js'],
+  migrations: ['dist/migration/**/*.js'],
+  subscribers: ['dist/subscriber/**/*.js'],
   cli: {
-    entitiesDir: 'src/entity',
-    migrationsDir: 'src/migration',
-    subscribersDir: 'src/subscriber',
+    entitiesDir: 'dist/entity',
+    migrationsDir: 'dist/migration',
+    subscribersDir: 'dist/subscriber',
   },
 };
